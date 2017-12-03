9497fb5a5a6b10e157d26fe1a3c6486ffe1b6b3b
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-12-02 17:04:08 -0800

    Save JWT token in localStorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = "http://localhost:3000";
 
 export function signinUser( { username, password } ){
-  return function(dispatch){
-    axios.post(`${ROOT_URL}/user/authenticate`, { username, password })
+  return function( dispatch ){
+    axios.post( `${ROOT_URL}/user/authenticate`, { username, password } )
       .then( response => {
-        browserHistory.push('/feature');
-      })
-      .catch({
 
-      });
+        dispatch( { type: AUTH_USER } );
+
+        localStorage.setItem( 'token', response.data.token );
+
+        browserHistory.push( '/feature' );
+      } )
+      .catch( {
+
+      } );
   }
 }
