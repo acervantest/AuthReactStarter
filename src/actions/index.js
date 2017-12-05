d7416056f3750fc09cc621d3cecb5a2de3c45cac
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-12-05 08:31:18 -0800

    Display error message

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+  AUTH_USER,
+  AUTH_ERROR
+} from './types';
 
 const ROOT_URL = "http://localhost:3000";
 
@@ -8,15 +11,19 @@ export function signinUser( { username, password } ){
   return function( dispatch ){
     axios.post( `${ROOT_URL}/user/authenticate`, { username, password } )
       .then( response => {
-
         dispatch( { type: AUTH_USER } );
-
         localStorage.setItem( 'token', response.data.token );
-
         browserHistory.push( '/feature' );
       } )
-      .catch( {
-
+      .catch( () => {
+        dispatch( authError( 'Bad login info!') );
       } );
   }
+}
+
+export function authError( error ){
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -9,7 +9,7 @@ class Signin extends Component {
       return (
         <div className="form-group">
           <label>{ field.label }</label>
-          <input className="form-control" type="text" { ...field.input }/>
+          <input className="form-control" type={ field.type } { ...field.input }/>
         </div>
       );
   }
@@ -19,20 +19,35 @@ class Signin extends Component {
       this.props.signinUser( { username, password } );
   }
 
+  renderAlert() {
+    if( this.props.errorMessage ){
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> { this.props.errorMessage }
+        </div>
+      );
+    }
+  }
+
   render() {
     const { handleSubmit, fields: { email, password } } = this.props;
     return (
       <form onSubmit={ handleSubmit( this.handleFormSubmit.bind( this ) ) }>
-        <Field label="Username" name="username" component={ this.renderField } />
-        <Field label="Password" name="password" component={ this.renderField } />
+        <Field label="Username" name="username" type="text" component={ this.renderField } />
+        <Field label="Password" name="password" type="password" component={ this.renderField } />
+          { this.renderAlert() }
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form >
     );
   }
 }
 
+function mapStateToProps( state ){
+  return { errorMessage: state.auth.error };
+}
+
 Signin = reduxForm({
   form: 'signin',
   fields: [ 'email', 'password' ]
 })(Signin);
-export default connect(null, actions)(Signin);
+export default connect(mapStateToProps, actions)(Signin);
